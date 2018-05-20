58996dccb043f1fcf8ab7d882f5938ed4221d039
SeventhDreamer <eternityisnow@gmail.com>
2018-05-20 16:06:45 -0500

    testing

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function searchWikipedia(){
+  $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + document.getElementById("query").value,
+      function(result) {
+        var html = "";
+      
+        for (var i = 0; i < result[1].length; i++) {
+          html += '<br><a target="_blank" href="' + result[3][i] + '"><div id="link"><h2>';
+          html += result[1][i];
+          html += "</h2><h4>" + result[2][i];
+          html += "</h4></div></a>";
+        }
+        $("#searchResult").html(html);
+      }
+    );
+}
+
+
+$(document).ready(function() {
+  document.getElementById('query').focus();
+  $(".search2").on("click", function() {
+    searchWikipedia();    
+  });
+  $(".search1").on('keyup', function(a){
+    if (a.keyCode ==13)
+      searchWikipedia();
+  });
+});
